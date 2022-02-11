d05c8e37aca887d8ecc00ce71e078a507029cfdf
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2022-02-11 12:28:33 -0300

    code

--- script.js
+++ script.js
@@ -3,8 +3,23 @@ const tabContentItems = document.querySelectorAll('.tab-content-item');
 
 // Select Tab Content
 function selectItem(e) {
+    // Remove the border before add another border
+    removeBorder();
+    removeShow();
     // Add border to the current tab selected
     this.classList.add('tab-border')
+    // Grab Content Item
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    // Add show
+    tabContentItem.classList.add('show');
+}
+
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'));
 }
 
 // Tab Click
